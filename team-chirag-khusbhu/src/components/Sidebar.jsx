73fb2d29d04daaf8cf09b1037d18c2b4f17e3458
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-02-04 23:10:23 +0530

    base UI ready

--- team-chirag-khusbhu/src/components/Sidebar.jsx
+++ team-chirag-khusbhu/src/components/Sidebar.jsx
@@ -1,11 +1,58 @@
+import { Link } from "react-router-dom";
 
- function Sidebar() {
+function Sidebar() {
   return (
     <>
-     <div className="sidebar">
-      Sidebar
-     </div>
+      <aside className="sidebar">
+        <div>
+          <div className="profile">
+            <img
+              src="https://images.pexels.com/photos/6918619/pexels-photo-6918619.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="human"
+            />
+          </div>
+          <h3>User Name</h3>
+          <h4>Designation</h4>
+        </div>
+
+        <nav>
+          <ul>
+            <Link to="/">
+              <i class="ri-home-4-fill"></i>
+              <span>Home</span>
+            </Link>
+            <Link to="/courses">
+              <i class="ri-home-4-fill"></i>
+              <span>Courses</span>
+            </Link>
+            <Link to="/events">
+              <i class="ri-home-4-fill"></i>
+              <span>Events</span>
+            </Link>
+            <Link to="/blogs">
+              <i class="ri-home-4-fill"></i>
+              <span>Blogs</span>
+            </Link>
+          </ul>
+        </nav>
+        <nav>
+          <ul>
+          <Link to="/">
+            <i class="ri-home-4-fill"></i>
+            <span>Settings</span>
+          </Link>
+          <Link to="/">
+            <i class="ri-home-4-fill"></i>
+            <span>Services</span>
+          </Link>
+          <Link to="/">
+            <i class="ri-home-4-fill"></i>
+            <span>Helps and FAQ</span>
+          </Link>
+          </ul>
+        </nav>
+      </aside>
     </>
-  )
+  );
 }
 export default Sidebar;
