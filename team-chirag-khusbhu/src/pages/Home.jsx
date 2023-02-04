5e69b65df308c41507d0f71f7d987a1bc21d4ae1
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-02-04 16:27:22 +0530

    A carousel added

--- team-chirag-khusbhu/src/pages/Home.jsx
+++ team-chirag-khusbhu/src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Card from '../components/Card'
 
 const Home = () => {
   return (
     <div>
-      Home
+      <div className='heading'>
+        <h2>Events</h2>
+        <p>View all</p>
+      </div>
+      <div className="carousel">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
     </div>
   )
 }
