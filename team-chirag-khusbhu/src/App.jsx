042683c4436330bbf2fa23c9742431451acf4e31
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-02-04 15:15:20 +0530

    Layout made

--- team-chirag-khusbhu/src/App.jsx
+++ team-chirag-khusbhu/src/App.jsx
@@ -1,4 +1,8 @@
+import { Routes,Route } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import {Home,Events,Courses,Blogs} from './pages'
 
 export default function App(){
   return(
@@ -6,6 +10,17 @@ export default function App(){
       <div className="gradient ball-1"></div>
       <div className="gradient ball-2"></div>
       <div className="gradient ball-3"></div>
+      <Sidebar/>
+      <main>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/events' element={<Events/>} />
+          <Route path='/courses' element={<Courses/>} />
+          <Route path='/blogs' element={<Blogs/>} />
+
+        </Routes>
+      </main>
     </>
   )
 }
